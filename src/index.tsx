8959d8eb648e09670ce0b68e7585b66497bf3e52
admin <505316314@qq.com>
2021-12-06 20:57:27 +0800

    第二次

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+const X=(
     <React.StrictMode>
         <App/>
-    </React.StrictMode>,
-    document.getElementById('root')
-);
+    </React.StrictMode>
+)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+const Y=document.getElementById('root')
+
+const render=()=>{
+    console.log(3121);
+    ReactDOM.render(X,Y);
+}
+
+render()
+
 reportWebVitals();
